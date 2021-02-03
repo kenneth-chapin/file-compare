1ac81a6f631808d5aeede0b17ec1388bc8800987
kenc <kenc@chapin.com>
2021-02-03 07:00:38 -0800

    When adjacent entries have same file size after sorting cumpute their hash vaules in a thread. Walk the list later looking for similar and non-'0' sum values.

--- dumplicates.py
+++ dumplicates.py
@@ -16,11 +16,11 @@ class file_info:
 
 def get_sum(finfo):
     try:
+        assert(finfo.sum == '0')
         with open(finfo.name, 'rb') as fd:
-            data = fd.read()
-            finfo.sum = hashlib.blake2b(data).hexdigest()
+            finfo.sum = hashlib.blake2b(fd.read()).hexdigest()
     except (PermissionError, IOError) as err:
-        print('{}: {}'.format(finfo.name, err))
+        print('{}'.format(err))
 
 
 def get_file_list(dirname, ext, files):
@@ -54,18 +54,27 @@ def get_file_infos(dir, ext):
     print_timing(start, 'find files')
 
     start = time.perf_counter_ns()
-    with ThreadPoolExecutor(max_workers=3) as executor:
+    with ThreadPoolExecutor(max_workers=8) as executor:
         for i in range(0, len(files) - 1):
-            if files[i].size == files[i+1].size:
-                executor.submit(get_sum, (files[i]))
+            nfile = files[i+1]
+            if files[i].size == nfile.size:
+                if files[i].sum == '0':
+                    executor.submit(get_sum, (files[i]))
+                executor.submit(get_sum, (nfile))
     print_timing(start, 'compute sums')
     return files
+
+
+def same(f0, f1):
+    if f0.sum != '0' and f1.sum == f0.sum:
+        return True
+    return False
 
 
 def show_dupes(finfos):
     dupes = []
     for i in range(0, len(finfos) - 1):
         finfo = finfos[i]
-        if finfo.sum != '0' and finfo.sum == finfos[i+1].sum:
+        if same(finfo, finfos[i+1]):
             dupes.append(finfo)
     return dupes
